a26c904024a8d767cbe1799125302eb45d30a416
Adarsh-Nagar-Ops <adarshnagar247@gmail.com>
2025-08-06 19:08:13 +0530

    Pushing code

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss=new WebSocketServer({port:8080});
+
+interface User{
+    socket:WebSocket,
+    room:string,
+    userId:string
+}
+
+const allSockets:User[]=[];
+let userCount=0;
+
+wss.on('connection',(socket)=>{
+    userCount=userCount+1; 
+    console.log('User connected #'+userCount);    
+    const userId=`user-${userCount}`
+    socket.on('message',(message)=>{
+        const parsedMessage=JSON.parse(message.toString());
+        
+        if(parsedMessage.type==='join'){
+            const roomId=parsedMessage.payload.roomId;
+            allSockets.push({socket, room:roomId, userId})
+        }
+
+        if(parsedMessage.type==='chat'){
+            //@ts-ignore
+            const roomId=allSockets.find(user=> user.socket===socket).room;
+            allSockets.forEach(user=>{
+                if(user.room===roomId){
+                    user.socket.send(JSON.stringify({
+                        "type":"chat",
+                        "payload":{
+                            "message":parsedMessage.payload.message,
+                            "senderId":parsedMessage.payload.senderId
+                        }
+                    }))
+                }
+            })
+        }
+    })
+})
